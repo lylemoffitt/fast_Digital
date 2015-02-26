6734ff7fca9cb0527100bb5859e6c1c9ef439952
lylemoffitt <lyle@moffitt.com>
2015-02-26 04:55:16 -0500

    Updated the fast_Digital.ino example file.

--- .ino.cpp
+++ .ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-02-26 04:22:49
+//This file has been generated on 2015-02-26 04:54:32
 
 #include "Arduino.h"
 #include "fast_Digital.h"
--- fast_Digital.ino
+++ fast_Digital.ino
@@ -1,41 +1,50 @@
 #include "fast_Digital.h"
 
+#define LHS PORTD
+#define RHS 42
+
+#define DATATYPE Bit_Mask
+
 MMIO<(uint16_t)&PORTD,Bit_Mask>
 #undef PORTD
 PORTD;
-MMIO<(uint16_t)&DDRD,Bit_Mask>
-#undef DDRD
-DDRD;
-MMIO<(uint16_t)&PIND,Bit_Mask>
-#undef PIND
-PIND;
-
-MMIO<(uint16_t)&TWCR,Bit_Mask>
-#undef TWCR
-TWCR;
-MMIO<(uint16_t)&TWSR,Bit_Mask>
-#undef TWSR
-TWSR;
-MMIO<(uint16_t)&TWBR,Bit_Mask>
-#undef TWBR
-TWBR;
-MMIO<(uint16_t)&TWDR,Bit_Mask>
-#undef TWDR
-TWDR;
-
 
 
 
 void setup()
 {
-	DDRD	=	_B{0,3,5,7};
-	PORTD	=	PORTD ^ 0xf0;
+	PORTD	=	_B{0,3,5,7};
+
 	PORTD	=	~PORTD;
+
 	PORTD->b0 = true;
-	PORTD	^=	0xff;
-	PORTD	=	PORTD + 23;
-	if( PORTD && false ){
-	}
+	PORTD->upper_nibble = 0xF;
+
+	PORTD	=	LHS +  RHS;
+	PORTD	=	LHS -  RHS;
+	PORTD	=	LHS *  RHS;
+	PORTD	=	LHS /  RHS;
+	PORTD	=	LHS %  RHS;
+	PORTD	=	LHS ^  RHS;
+	PORTD	=	LHS &  RHS;
+	PORTD	=	LHS |  RHS;
+	PORTD	=	LHS << RHS;
+	PORTD	=	LHS >> RHS;
+
+	PORTD	+=	RHS;
+	PORTD	-=	RHS;
+	PORTD	*=	RHS;
+	PORTD	/=	RHS;
+	PORTD	%=	RHS;
+	PORTD	&=	RHS;
+	PORTD	|=	RHS;
+	PORTD	^=	RHS;
+	PORTD	<<=	RHS;
+	PORTD	>>=	RHS;
+
+
+	if( PORTD && false );
+	if( !PORTD && false );
 
 }
 
